3e212d40da8a21af0e0fa170f0a6b6852be47966
Satish Reddy <yettapusatishreddy@email.com>
2025-06-30 12:07:00 +0530

    Nav Bar Developed Successfully

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,64 @@
-import AmazonLogo from '../../assets/Logos/amazon-logo.png'
-import React, { useState } from 'react'
-import './navbar.css'
+import AmazonLogo from "../../assets/Logos/amazon-logo.png";
+import React, { useState } from "react";
+import "./navbar.css";
 const Navbar = () => {
-  let [menuOpen, setMenuOpen] = useState(false);
+  let [menuOpen, setMenuOpen] = useState(true);
   return (
-    <nav className='h-[10%] w-screen bg-transparent box-border relative flex items-center justify-between px-[10%] py-[2%] '>
-      <div><img src={AmazonLogo} alt="" className='h-[30%] w-[15%]' /></div>
-     <button className='menu-button' onClick={()=>{setMenuOpen(!menuOpen)}}>{menuOpen?'✖' : '☰'}</button>
-      <ul className={`flex w-[60%] h-auto items-left justify-end nav-items ${menuOpen?"menuButtons":""}`}>
-        <li className='border-box h-[100%] w-[20%] p-[1%] bg-transaparent text-center'><a href="" className='text-center font-thin font-weight: 200'>About Us</a></li>
-        <li className='border-box h-[100%] w-[20%] p-[1%] bg-transaparent text-center'><a href="" className='text-center font-thin font-weight: 500'>Services</a></li>
-        <li className='border-box h-[100%] w-[20%] p-[1%] bg-transaparent text-center'><a href="" className='text-center font-thin font-weight: 500' >Courses</a></li>
-        <li className='border-box h-[100%] w-[20%] p-[1%] bg-transaparent text-center'><a href="" className='text-center font-thin font-weight: 500'>Contact Us</a></li>
-        <li className='border-box h-[100%] w-[20%] p-[1%] bg-orange-400 text-center'><a href="" className='text-center font-thin font-weight: 500'>Enroll Now</a></li>
-      </ul>
+    <nav className="h-auto w-screen bg-transparent px-20 flex flex-col box-border">
+      <div className="h-[70px] w-[100%] py-2 bg-transparent flex justify-between">
+        <img src={AmazonLogo} alt="Logo" className="h-[100%] w-[15%] bolck" />
+        <ul className="w-[auto] bg-transparent flex gap-x-4 items-center web-menu">
+          <li className="bg-transparent p-2">
+            <a href="">Home</a>
+          </li>
+          <li className="bg-transparent p-2">
+            <a href="">About Us</a>
+          </li>
+          <li className="bg-transparent p-2">
+            <a href="">Contact Us</a>
+          </li>
+          <li className="bg-amber-600 p-2">
+            <a href="">Register Now</a>
+          </li>
+        </ul>
+        <button
+          id="menuBtn"
+          class="text-2xl font-bold p-2 menu-icon"
+          onClick={() => {
+            setMenuOpen(!menuOpen);
+          }}
+        >
+          {menuOpen ? (
+            <span id="menuIcon">☰</span>
+          ) : (
+            <span id="closeIcon" className="closeIcon">
+              ✖
+            </span>
+          )}
+        </button>
+      </div>
+      <div
+        className={` mobile-menu ${
+          !menuOpen ? "mobile-menu-block" : "mobile-menu-none"
+        } h-auto w-full bg-transparent`}
+      >
+        <ul className="w-[auto] bg-transparent flex flex-col gap-x-4 items-center">
+          <li className="bg-transparent p-2">
+            <a href="">Home</a>
+          </li>
+          <li className="bg-transparent p-2">
+            <a href="">About Us</a>
+          </li>
+          <li className="bg-transparent p-2">
+            <a href="">Contact Us</a>
+          </li>
+          <li className="bg-amber-600 p-2">
+            <a href="">Register Now</a>
+          </li>
+        </ul>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
